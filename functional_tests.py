b0e3e1c15e375ffffcf39a3cecbdf5bfd71ea873
Alexey Lebedeff <binarin@gmail.com>
2014-07-07 08:05:13 +0400

    wip

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
 from os import environ as env
 import unittest
 
@@ -32,15 +33,26 @@ class NewVisitorTest(SeleniumMixin, unittest.TestCase):
         # Edith has heard about a cool new online to-do app. She goes to check
         # out it's homepage.
         self.browser.get(self.url)
+
+        # She notices the page title and header mention to-do list
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1')
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter to-do item straight away.
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby is
         # tying fly-fishing lures)
+        input_box.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists "1:
         # Buy peacock feathers" as an item in a to-do list.
+        input_box.send_keys(Keys.ENTER)
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
