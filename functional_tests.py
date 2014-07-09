cbc2745ad9ddafcb6059b4486b11927591cae7b6
Alexey Lebedeff <binarin@gmail.com>
2014-07-09 07:56:01 +0400

    Functional tests refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -44,6 +44,11 @@ class SeleniumMixin:
 
 
 class NewVisitorTest(SeleniumMixin, unittest.TestCase):
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     @SeleniumMixin.screenshot_on_fail
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check
@@ -70,11 +75,7 @@ class NewVisitorTest(SeleniumMixin, unittest.TestCase):
         # Buy peacock feathers" as an item in a to-do list.
         input_box.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Buy peacock feathers', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -84,14 +85,8 @@ class NewVisitorTest(SeleniumMixin, unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '1: Buy peacock feathers', [row.text for row in rows]
-        )
-        self.assertIn(
-            '2: Use peacock feathers to make a fly', [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. The she sees
         # that the site has generated a unique URL for her -- there is some
