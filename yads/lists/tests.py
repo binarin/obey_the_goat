d4c844ceaaed65f36a2558cb176786d3534a09fd
Alexey Lebedeff <binarin@gmail.com>
2014-07-10 22:02:07 +0400

    wip

--- yads/lists/tests.py
+++ yads/lists/tests.py
@@ -29,9 +29,6 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
     def test_home_page_redirects_after_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -40,19 +37,26 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(
+            response['location'], '/lists/the-only-list-in-the-world/'
+        )
 
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
+
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
         Item.objects.create(text='Itemey 1')
         Item.objects.create(text='Itemey 2')
 
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('Itemey 1', response.content.decode())
-        self.assertIn('Itemey 2', response.content.decode())
+        self.assertContains(response, 'Itemey 1')
+        self.assertContains(response, 'Itemey 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'lists/list.html')
