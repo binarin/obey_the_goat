1c2b353831284c722558b7ad54c87316df3874bc
Alexey Lebedeff <binarin@gmail.com>
2014-07-10 10:39:20 +0400

    Functional tests for multiple lists

--- yads/functional_tests/tests.py
+++ yads/functional_tests/tests.py
@@ -20,9 +20,9 @@ class SeleniumMixin:
                 raise
         return wrapper
 
-    def setUp(self):
-        super().setUp()
-        self.take_screenshot = False
+    def reopen_browser(self):
+        if self.browser:
+            self.browser.quit()
         self.browser = webdriver.Remote(
             'http://{}:{}/wd/hub'.format(
                 env['SELENIUM_PORT_4444_TCP_ADDR'],
@@ -30,6 +30,13 @@ class SeleniumMixin:
             ),
             desired_capabilities=DesiredCapabilities.CHROME
         )
+
+
+    def setUp(self):
+        super().setUp()
+        self.take_screenshot = False
+        self.browser = None
+        self.reopen_browser()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
@@ -68,10 +75,12 @@ class NewVisitorTestCase(SeleniumMixin, LiveServerTestCase):
         # tying fly-fishing lures)
         input_box.send_keys("Buy peacock feathers")
 
-        # When she hits enter, the page updates, and now the page lists "1:
-        # Buy peacock feathers" as an item in a to-do list.
+        # When she hits enter, she is taken to a new URL, and now the
+        # page lists "1: Buy peacock feathers" as an item in a to-do
+        # list table.
         input_box.send_keys(Keys.ENTER)
-
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -85,11 +94,30 @@ class NewVisitorTestCase(SeleniumMixin, LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # Edith wonders whether the site will remember her list. The she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
+        # Now a new user, Francis, comes along to the site.
+        self.reopen_browser()
 
-        # She visits that URL - her to-do list is still there.
+        # Francis visits the home page. There is no sign of Edith's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tage_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
 
-        # Satisfied, she goes back to sleep.
+        # Francis starts a new list by entering a new item. He is less
+        # interesting than Edith.
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys("Buy milk")
+        input_box.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tage_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep.
         self.fail("Finish the test!")
